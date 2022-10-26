03cd22e959ab7949137284a6050b12b6975e0983
TannerGlazier <tannerglazier@gmail.com>
2022-10-26 13:40:29 -0600

    Controller page

---
+++ ClassProject/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassProject.Models;
+using ClassProject.Models.ViewModels;
+
+namespace ClassProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IClassProjectRepository repo;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
