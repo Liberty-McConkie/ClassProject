039f839feaf8271b0e1490338093dcc88a1f7f06
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2022-10-26 13:42:50 -0600

    models almost complete

--- ClassProject/Startup.cs
+++ ClassProject/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ClassProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,12 @@ namespace ClassProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddDbContext<StudentInfoContext>(options =>
+            {
+                //fix this (:
+                options.UseSqlite(Configuration["ConnectionStrings:StudentsDbConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
